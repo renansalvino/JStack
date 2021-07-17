b9a51da9890a55bc9ffb47c98224d58621d94c51
renansalvino <renansalvinomartins454@gmail.com>
2021-07-17 02:07:15 -0300

    update

--- firstpass_nodejs/firstapi/src/index.js
+++ firstpass_nodejs/firstapi/src/index.js
@@ -4,9 +4,9 @@ const http = require('http');
 const server = http.createServer((request, response) => {
 console.log(`Request method: ${request.method} | Endopoint: ${request.url}`);
 
+  if (reques)
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.end('<h1>Ola mundo!<h1>');
 });
 
 server.listen(3000, () => console.log('🔥 Server started at http://localhost:3000')); 
-
