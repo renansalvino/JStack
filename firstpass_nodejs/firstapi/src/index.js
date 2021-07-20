e90ccae0e9890c6d829843edb57cd076490e0fe0
renansalvino <renansalvinomartins454@gmail.com>
2021-07-20 00:17:05 -0300

    Aula: Criando a primeira API (feita)

--- firstpass_nodejs/firstapi/src/index.js
+++ firstpass_nodejs/firstapi/src/index.js
@@ -1,12 +1,22 @@
 //Importando módulo HTTP
 const http = require('http');
+const users = require('./mocks/users');
+
 
 const server = http.createServer((request, response) => {
-console.log(`Request method: ${request.method} | Endopoint: ${request.url}`);
+  console.log(`Request method: ${request.method} | Endopoint: ${request.url}`);
 
-  if (reques)
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('<h1>Ola mundo!<h1>');
+  if (request.url === '/users' && request.method === 'GET') {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    //Dentro do END sempre tenho que mandar uma string
+    response.end(JSON.stringify(users));
+  }else {
+    response.writeHead(404, {'Content-Type': 'text/html'});
+    response.end(`Cannot ${request.method} ${request.url}`);
+  }
+  
+  // response.writeHead(200, { 'Content-Type': 'text/html' });
+  // response.end('<h1>Ola mundo!<h1>');
 });
 
 server.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'))
---
+++ firstpass_nodejs/firstapi/src/mocks/users.js
@@ -0,0 +1,17 @@
+module.exports = [
+
+  {
+    id:1,
+    name: 'Renan',
+  },
+  {
+    id:2,
+    name: 'Rodrigo',
+  },
+  {
+    id:1,
+    name: 'Rute',
+  }
+
+
+]
